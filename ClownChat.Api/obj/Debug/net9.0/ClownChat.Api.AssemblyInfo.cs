bfda26829f5e13042830a8478ae0e663f3c01d9b
AmeerAtia <amir.gppume@gmail.com>
2025-03-07 22:12:06 +0200

    test

--- ClownChat.Api/obj/Debug/net9.0/ClownChat.Api.AssemblyInfo.cs
+++ ClownChat.Api/obj/Debug/net9.0/ClownChat.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClownChat.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afab167d7e36e80bc5e8b54df13178a4fb6590f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClownChat.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClownChat.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
